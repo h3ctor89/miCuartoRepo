9000318344167b420ff8c13ae755bf624ec9a7b1
Hector Gutierrez <gutierrez.h3ctor@gmail.com>
2025-06-08 23:25:09 -0600

    método de platzi agregado

--- API_Python/app.py
+++ API_Python/app.py
@@ -16,5 +16,13 @@ def get_superheroes():
     return rows
 
 
+@app.get("/superheroesDC")
+def get_superheroes():
+    rows = ["Superman", "Batman", "Flash", "Linterna Verde", "Mujer maravilla", "Aquaman", "Shazam", "Cyborg"]
+    return rows
 
 
+@app.get("/cursosPlatzi")
+def get_cursos():
+    rows = ["Docker", "Bash", "Linux", "Inglés", "Python", "Javascript", "API"]
+    return rows
